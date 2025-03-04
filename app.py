32bc2955a391d7a85dfe92a7d49d0f42abf0276f
C0N5OLE <82925414+C0N5OLE@users.noreply.github.com>
2025-03-04 12:08:50 +0000

    Lots of updates

--- app.py
+++ app.py
@@ -1,91 +1,135 @@
-from flask import Flask, jsonify, request, render_template
-from flask import request
-import json
+'''Web app'''
 import sqlite3
+from flask import Flask, request, render_template, session, redirect
 app = Flask(__name__)
-
+app.secret_key = '6MT7DbpbBnGeOKC0HzthJVngnpBmne7v'
 
 @app.route('/', methods=['POST'])
 def post():
-    if request.form['button']=="CONTINUE":  
+    '''handling the login requests'''
+    users = sqlite3.connect('database/data.db').cursor().execute("SELECT * FROM users").fetchall()
+    sqlite3.connect('database/data.db').close()
+    if request.form['button']=="CONTINUE":
         username = request.form['username']
         password = request.form['password']
         if username.lower()=="log":
             print(users)
-        LoggedIn = False
+        logged_in = False
         for x in users:
-            if x['Username'].lower()==username.lower():
-                if x['Password']==password:
+            if x[1].lower()==username.lower():
+                if x[2]==password:
                     print(f"User logged in as {username}")
-                    return(render_template('index.html', name=username))
-        if LoggedIn==False:
-            return render_template("login.html")
+                    session['logged_in'] = True
+                    if username=="jeremy":
+                        session['admin']=True
+                return redirect('/')
+        if logged_in is False:
+            return render_template("login.html", error="Invalid Username or Password")
     elif request.form['button']=="CREATE ACCOUNT":
         username = request.form['username']
         password = request.form['password']
         invalid = False
         for i in users:
-            if i['Username'].lower()==username.lower():
+            if i[1].lower()==username.lower():
                 invalid=True
-        if invalid==False:
-            users.append({"Username": username, "Password": password, "ID": 0})
-            json.dump(users, open("Users.json", "w"), indent=2)
-            return "Account created, Username: " + username +" Password: " + password
-        else:
-            return render_template('signup.html')
+        if invalid is False:
+            con = sqlite3.connect('database/data.db')
+            cur = con.cursor()
+            cur.execute("INSERT INTO users(username, password, email) VALUES (?, ?, ?)"
+                        , (username, password, "testemail"))
+            con.commit()
+            con.close()
 
-#handling GET requests to pages
-@app.route('/login.html', methods=['GET'])
-def showlogin():
+            return render_template('verification.html')
+        return render_template('signup.html', error="Username is already in use")
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    '''redirect to login.html'''
     return render_template('login.html')
-@app.route('/signup.html', methods=['GET'])
-def showsignup():
-    return render_template('signup.html')
-@app.route('/about.html', methods=['GET'])
-def showabout():
-    return render_template('about.html')
-@app.route('/place.html', methods=['GET'])
-def showplace():
-    return render_template('place.html')
-@app.route('/postplace.html', methods=['GET'])
-def showpostscreen():
-    return render_template('postplace.html')
-@app.route('/category.html', methods=['GET'])
-def showcategory():
-    return render_template('category.html')
 
-#handle post requests
-@app.route('/place.html', methods=['POST'])
-def postplace():
-    cat = request.form['button']
-    query = f"SELECT * FROM catalogue WHERE place_id={cat}"
-    print(query)
-    print(sqlite3.connect('database/data.db').cursor().execute(query).fetchall())
-    return render_template('place.html', category=request.form['button'], catalogue=sqlite3.connect('database/data.db').cursor().execute(query).fetchall(), close=sqlite3.connect('database/data.db').close())
-@app.route('/category.html', methods=['POST'])
-def postcategory():
-    return render_template('category.html', category=request.form['button'], catalogue=sqlite3.connect('database/data.db').cursor().execute("SELECT * FROM catalogue"), close=sqlite3.connect('database/data.db').close())
-@app.route('/postplace.html', methods=['POST'])
+@app.route('/signup', methods=['GET'])
+def signup():
+    '''redirect to signup.html'''
+    return render_template('signup.html')
+
+@app.route('/logout')
+def logout():
+    '''redirect delete session cookies and redirect to login.html'''
+    session.pop('logged_in', None)
+    session.pop('admin', None)
+    return render_template('login.html')
+
+@app.route('/verification')
+def verification():
+    '''redirect to verification.html'''
+    return render_template('verification.html')
+
+@app.route('/about', methods=['GET'])
+def aboutpage():
+    '''redirect to about.html'''
+    return render_template('about.html')
+
+@app.route('/place', methods=['GET'])
+def showplace():
+    '''redirect to place.html'''
+    query = f"SELECT * FROM catalogue WHERE place_id={request.args.get('place_id')}"
+    return render_template('place.html',
+                           catalogue=sqlite3.connect('database/data.db').cursor()
+                           .execute(query).fetchall(),
+                           close=sqlite3.connect('database/data.db').close())
+
+@app.route('/postplace', methods=['GET'])
+def postscreen():
+    '''redirect to postplace.html'''
+    return render_template('postplace.html')
+
+@app.route('/category', methods=['GET'])
+def showcategory():
+    '''get the category's relevant info and return it'''
+    return render_template('category.html',
+                           category=request.args.get("category"),
+                           catalogue=sqlite3.connect('database/data.db').cursor().execute(
+                               "SELECT * FROM catalogue"),
+                           close=sqlite3.connect('database/data.db').close())
+
+@app.route('/postplace', methods=['POST'])
 def postdata():
+    '''get the info from the post form and update the database to include it. refresh the page'''
     place_id = int(request.form['place_id'])
     name = request.form['name']
     category = request.form['category']
     coordx = float(request.form['coordx'])
     coordy = float(request.form['coordy'])
-    about = request.form['about']
     address = request.form['address']
     open_hours = request.form['open_hours']
     website = request.form['website']
     phone_number = request.form['phone_number']
+    rating = request.form['rating']
     con = sqlite3.connect('database/data.db')
     cur = con.cursor()
-    cur.execute("INSERT INTO catalogue(place_id, name, category, coordx, coordy, about, address, open_hours, website, phone_number) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (place_id, name, category, coordx, coordy, about, address, open_hours, website, phone_number))
+    cur.execute("INSERT INTO catalogue(place_id, name, category,"
+                "coordx, coordy, address, open_hours, website, phone_number, rating"
+                ") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                (place_id, name, category, coordx, coordy,
+                 address, open_hours, website, phone_number, rating))
     con.commit()
+    con.close()
     return render_template("postplace.html")
 
 @app.route('/', methods=['GET'])
-def get():
-    return render_template('index.html')
+def home():
+    '''redirect to index.html if logged in and login.html if not'''
+    try:
+        return render_template('index.html', 
+                               signed_in=session['logged_in'], 
+                               view=request.args.get("view"), 
+                               catalogue=sqlite3.connect('database/data.db').cursor()
+                                .execute("SELECT * FROM catalogue").fetchall(),
+                                close=sqlite3.connect('database/data.db').close())
+    except KeyError:
+        return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
